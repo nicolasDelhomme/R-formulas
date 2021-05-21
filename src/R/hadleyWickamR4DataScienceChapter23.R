f39fe9abae6835161a417803fec5c87757199ccb
nicolasDelhomme <nicolas.delhomme@umu.se>
2021-05-21 16:12:48 +0200

    Week 4 recap walk through

--- src/R/hadleyWickamR4DataScienceChapter23.R
+++ src/R/hadleyWickamR4DataScienceChapter23.R
@@ -1,40 +1,64 @@
-# 23.1
-library(tidyverse)
-library(modelr)
-options(na.action = na.warn)
+#' ---
+#' title: Hadley Wickham's R for data science chapter 23
+#' author: Nicolas Delhomme
+#' date: "`r Sys.Date()`"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    number_sections: true
+#' ---
+#' # 23.1
 
-# 23.2
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(modelr)
+  options(na.action = na.warn)
+})
+
+#' # 23.2
+#' Take a first look at the data
 ggplot(sim1, aes(x, y)) + 
   geom_point()
 
+#' The data looks linear, let's try to model it
 models <- tibble(
   a1 = runif(250, -20, 40),
   a2 = runif(250, -5, 5)
 )
 
+#' we get 250 models, some good, some bad
 ggplot(sim1, aes(x, y)) + 
   geom_abline(aes(intercept = a1, slope = a2), data = models, alpha = 1/4) +
   geom_point() 
 
+#' write the model as a function
 model1 <- function(a, data) {
   a[1] + data$x * a[2]
 }
+
+#' Select a model as an example
 model1(c(7, 1.5), sim1)
 
+#' Create a function to calculate the residuals
 measure_distance <- function(mod, data) {
   diff <- data$y - model1(mod, data)
   sqrt(mean(diff ^ 2))
 }
+
+# calculate it for our example
 measure_distance(c(7, 1.5), sim1)
 
+# create another function to calculate the distance for any a1 and a2
 sim1_dist <- function(a1, a2) {
   measure_distance(c(a1, a2), sim1)
 }
 
+#' Calculate the distance foe all models
 models <- models %>% 
   mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
 models
 
+#' Visualise the ten best models
 ggplot(sim1, aes(x, y)) + 
   geom_point(size = 2, colour = "grey30") + 
   geom_abline(
@@ -42,21 +66,25 @@ ggplot(sim1, aes(x, y)) +
     data = filter(models, rank(dist) <= 10)
   )
 
+#' Visualise the data differently, look at the parameters, colored by the distance
 ggplot(models, aes(a1, a2)) +
   geom_point(data = filter(models, rank(dist) <= 10), size = 4, colour = "red") +
   geom_point(aes(colour = -dist))
 
+#' Use a less random approach, use a rectangular grid for the parameters
 grid <- expand.grid(
   a1 = seq(-5, 20, length = 25),
   a2 = seq(1, 3, length = 25)
 ) %>% 
   mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
 
+#' plot the grid
 grid %>% 
   ggplot(aes(a1, a2)) +
   geom_point(data = filter(grid, rank(dist) <= 10), size = 4, colour = "red") +
   geom_point(aes(colour = -dist)) 
 
+#' These models are all a better fit than the previous approach
 ggplot(sim1, aes(x, y)) + 
   geom_point(size = 2, colour = "grey30") + 
   geom_abline(
@@ -64,33 +92,42 @@ ggplot(sim1, aes(x, y)) +
     data = filter(grid, rank(dist) <= 10)
   )
 
+#' Instead a manual approach, use functions to optimise
 best <- optim(c(0, 0), measure_distance, data = sim1)
 best$par
 
+#' plot that best fit
 ggplot(sim1, aes(x, y)) + 
   geom_point(size = 2, colour = "grey30") + 
   geom_abline(intercept = best$par[1], slope = best$par[2])
 
+#' The fit looks good
+#' What we  did is the same as running the lm function
 sim1_mod <- lm(y ~ x, data = sim1)
+
+#' Yay! we reproduce the lm function!
 coef(sim1_mod)
 summary(sim1_mod)
 
-
-# Exercises
-# 23.2.1
-# One downside of the linear model is that it is sensitive to unusual values 
-# because the distance incorporates a squared term. Fit a linear model to the 
-# simulated data below, and visualise the results. Rerun a few times to 
-# generate different simulated datasets. What do you notice about the model?
+#' ## Exercises
+#' ### 23.2.1
+#' One downside of the linear model is that it is sensitive to unusual values 
+#' because the distance incorporates a squared term. Fit a linear model to the 
+#' simulated data below, and visualise the results. Rerun a few times to 
+#' generate different simulated datasets. What do you notice about the model?
+#' 
+#' Let's generate values drown from the t distribution (for the intersect in y)
 sim1a <- tibble(
   x = rep(1:10, each = 3),
   y = x * 1.5 + 6 + rt(length(x), df = 2)
 )
 
+#' plot the data
 ggplot(sim1a, aes(x = x, y = y)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
+#' Create a new funtion to generate the data 
 simt <- function(i) {
   tibble(
     x = rep(1:10, each = 3),
@@ -99,13 +136,14 @@ simt <- function(i) {
   )
 }
 
+#' Generate 12 simulations
 sims <- map_df(1:12, simt)
 
+#' Plot them 
 ggplot(sims, aes(x = x, y = y)) +
   geom_point() +
   geom_smooth(method = "lm", colour = "red") +
   facet_wrap(~.id, ncol = 4)
-#> `geom_smooth()` using formula 'y ~ x'
 
 sim_norm <- function(i) {
   tibble(
@@ -135,34 +173,39 @@ pnorm(2, lower.tail = FALSE)
 
 pt(2, df=2, lower.tail = FALSE)
 
-# 23.2.2 One way to make linear models more robust is to use a different distance 
-# measure. For example, instead of root-mean-squared distance, you could use 
-# mean-absolute distance:
+#' 23.2.2 One way to make linear models more robust is to use a different distance 
+#' measure. For example, instead of root-mean-squared distance, you could use 
+#' mean-absolute distance:
 
 measure_distance <- function(mod, data) {
   diff <- data$y - model1(mod, data)
   mean(abs(diff))
 }
 
-# Use optim() to fit this model to the simulated data above and compare it to 
-# the linear model.
+#' Use optim() to fit this model to the simulated data above and compare it to 
+#' the linear model.
 
 make_prediction <- function(mod, data) {
   mod[1] + mod[2] * data$x
 }
 
+#' optimise the model using the mean-absolute distance
 best <- optim(c(0, 0), measure_distance, data = sim1a)
 best$par
 
+#' do the same for the root-mean-squared distance
 measure_distance_ls <- function(mod, data) {
   diff <- data$y - (mod[1] + mod[2] * data$x)
   sqrt(mean(diff^2))
 }
 
+#' optimise it
+#' 
+#' We can observe that the former is more conservative / more robust to outliers
 best <- optim(c(0, 0), measure_distance_ls, data = sim1a)
 best$par
 
-# 2.23.3
+#' ### 2.23.3
 # One challenge with performing numerical optimisation is that it’s only 
 # guaranteed to find one local optimum. What’s the problem with optimising a 
 # three parameter model like this?
@@ -171,12 +214,13 @@ model3 <- function(a, data) {
   a[1] + data$x * a[2] + a[3]
 }
 
-
+#' a function to calculate the distance
 measure_distance_3 <- function(a, data) {
   diff <- data$y - model3(a, data)
   sqrt(mean(diff^2))
 }
 
+#' optimise the model, initialising with different values
 best3a <- optim(c(0, 0, 0), measure_distance_3, data = sim1)
 best3a$par
 
@@ -186,32 +230,37 @@ best3b$par
 best3c <- optim(c(0, 0, 5), measure_distance_3, data = sim1)
 best3c$par
 
-# 23.3
-
+#' ## 23.3
+#' create a parameter
 grid <- sim1 %>% 
   data_grid(x) 
 grid
 
+#' add the prediction
 grid <- grid %>% 
   add_predictions(sim1_mod) 
 grid
 
+#' visualise it
 ggplot(sim1, aes(x)) +
   geom_point(aes(y = y)) +
   geom_line(aes(y = pred), data = grid, colour = "red", size = 1)
 
+#' add the residuals
 sim1 <- sim1 %>% 
   add_residuals(sim1_mod)
 sim1
 
+#' visualise as a polygon
 ggplot(sim1, aes(resid)) + 
   geom_freqpoly(binwidth = 0.5)
 
+#' visualise as a function
 ggplot(sim1, aes(x, resid)) + 
   geom_ref_line(h = 0) +
   geom_point() 
 
-# Exercises
+#' # Exercises
 # 23.3.1. Instead of using lm() to fit a straight line, you can use loess() to fit a 
 # smooth curve. Repeat the process of model fitting, grid generation, 
 # predictions, and visualisation on sim1 using loess() instead of lm(). 
@@ -220,6 +269,7 @@ ggplot(sim1, aes(x, resid)) +
 sim1_loess <- loess(y ~ x, data = sim1)
 sim1_lm <- lm(y ~ x, data = sim1)
 
+#' grid search for the loess model
 grid_loess <- sim1 %>%
   add_predictions(sim1_loess)
 
